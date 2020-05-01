e6fa7313e6fe5ae6fe8ed0a663c15dff6c0bafec
Saneek28 <64087643+Saneek28@users.noreply.github.com>
2020-05-01 08:08:40 +0300

    Задание 2

---
+++ sandbox/src/main/java/ru/sogaz/java/sandbox/DistanceOfPoint.java
@@ -0,0 +1,18 @@
+package ru.sogaz.java.sandbox;
+
+public class DistanceOfPoint
+{
+    public static void main(String[] args)
+    {
+        Point p1 = new Point(5, 6);
+        Point p2 = new Point(7, 8);
+        System.out.println("p1.x = " + p1.x + " , " + p1.y);
+        System.out.println("p2.x = " + p2.x + " , " + p2.y);
+        System.out.println("p1.distance(p2) = " + p1.distance(p2));
+    }
+
+    public static double distance(Point p1, Point p2)
+    {
+        return Math.sqrt( Math.pow((p2.y - p1.y), 2) +  Math.pow((p2.x - p1.x),2));
+    }
+}
