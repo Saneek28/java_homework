d7aedecd55082e52ea18c49793b7df2048a5b126
Saneek28 <64087643+Saneek28@users.noreply.github.com>
2020-05-01 15:38:56 +0300

    Добавление библиотек и удаление не нужных файлов

--- sandbox/src/main/java/ru/sogaz/java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sogaz/java/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package ru.sogaz.java.sandbox;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class MyFirstProgram {
 
 public static void main(String[] args) {
@@ -7,21 +9,14 @@ public static void main(String[] args) {
   maybe("User");
   maybe("Alexander");
   maybe("Prostakov");
-  double l = 5;
-  System.out.println("Площадь квадрата со стороной" + l+ "=" + area(l));
+  Square s = new Square(5);
+  System.out.println("Площадь квадрата со стороной" + s.l+ "=" + s.area());
 
-  double a = 4;
-  double b = 6;
-  System.out.println("Площадь прямоугольника со сторонами" + a + "и" + b + "=" + area(a, b));
+  Rectangle r = new Rectangle(4,6);
+  System.out.println("Площадь прямоугольника со сторонами" + r.a + "и" + r.b + "=" + r.area());
 }
 public static void maybe(String somebody) {
   System.out.println("Hello "+somebody+ "!");
-}
-public static double area(double len) {
-  return len*len;
-}
-public static double area(double a,double b) {
-  return a*b;
 }
 
 }
